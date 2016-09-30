7145b1f96105df2c31cb262f515de3ef81ddf816
JiaMingLin <robin890650@gmail.com>
2016-10-01 01:14:40 +0800

    add read hdfs data to df

--- common/importer.py
+++ common/importer.py
@@ -1,5 +1,8 @@
-from pyspark import SparkConf, SparkContext, SQLContext
-conf = SparkConf().setMaster("local").setAppName("de_identification")
-sc = SparkContext(conf = conf)
-sqlContext = SQLContext(sc)
-partition_num = 40
+try:
+	from pyspark import SparkConf, SparkContext, SQLContext
+	conf = SparkConf().setMaster("local").setAppName("de_identification")
+	sc = SparkContext(conf = conf)
+	sqlContext = SQLContext(sc)
+	partition_num = 40
+except:
+	print 'run from local python command'
---
+++ spark-exp/dep-graph-dist.py
@@ -0,0 +1,5 @@
+from common.base import *
+
+class DepGraphDist(Base):
+	def __init__(self, data):
+		pass
