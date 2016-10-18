a65c3cf27fe252f9566ece54b62daf2616043fdf
JiaMingLin <robin890650@gmail.com>
2016-10-19 01:01:35 +0800

    cache

---
+++ spark_exp/dptable_dist.py
@@ -0,0 +1,17 @@
+
+from common.base import *
+
+class DPTableDist(Base):
+	def __init__(self):
+		pass
+
+	def run(self):
+
+		# read data
+		# repartition
+		# build markov graph
+		# build junction tree
+		# merge cliques to reduce variance
+		# find histogramdds
+		# do inference
+		# sampling
