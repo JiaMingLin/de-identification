cdd6ed332109a8053ba48ba8a44b724ab2b39e94
JiaMingLin <robin890650@gmail.com>
2016-11-10 10:04:19 +0800

    fix bugs

--- R/jtree.R
+++ R/jtree.R
@@ -2,12 +2,9 @@ get_jtree <- function(edges, nodes, jtree_path, display=TRUE){
 	
 	jtreepy <- list()
 	if(length(edges) == 0){
-		jtreepy$cliques <- nodes
-		jtreepy$separators <- list()
-		jtreepy$parents <- list()
-
-		save_jtree(jtreepy)
-		return(jtreepy, jtree_path)
+		jtreepy <- gen_psudo_tree(nodes)
+		save_jtree(jtreepy, jtree_path)
+		return(jtreepy)
 	}
 
 	depgraph <- ugList(edges)
@@ -17,7 +14,6 @@ get_jtree <- function(edges, nodes, jtree_path, display=TRUE){
 	save_jtree(jtree, jtree_path)
 	
 	# convert to python readable format
-	
 	jtreepy$cliques <- jtree$cliques
 	jtreepy$separators <- jtree$separators
 	jtreepy$parents <- jtree$parents
@@ -28,4 +24,12 @@ save_jtree <- function(jtree, path){
 	if(nchar(path)>1){
 		saveRDS(jtree, file=path)	
 	}
+}
+
+gen_psudo_tree <- function(nodes){
+	tree <- list()
+	tree$cliques <- nodes
+	tree$separators <- lapply(1:length(nodes), function(x) character(0))
+	tree$parents <- sapply(1:length(nodes), function(x) 0)
+	return(tree)
 }
